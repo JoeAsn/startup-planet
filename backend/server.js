2954c5f742ed1953cdb97a37ca565c1614fed37b
Yohannes Asnake <asnakeyohannes095@gmail.com>
2026-09-24 09:56:55 +0300

    sort an object for based on the queryParams

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,23 @@
-import express from "express" ;
-const app = express() ;
-app.listen(5200 , ()=> console.log("the server is started on port 5200"))
+import express from "express";
+import { startups } from "./Data/data.js";
+const app = express();
+app.get("/api", (req, res) => {
+  // const url = new URL(req.url, `http://${req.headers.host}`);
+  // let searchParams = Object.fromEntries(url.searchParams)
+  // console.log(searchParams) to extract the search params from the pure node
+  console.log(req.query); //searchParams express ;
+  const queryParams = req.query;
+  let filteredData = startups.filter((data) => {
+    for (let query in queryParams) {
+      const key = query.toLowerCase();
+
+      if (!(key in data)) return false;
+      if (data[key] !== queryParams[query]) return false;
+    }
+
+    return true;
+  });
+  console.log(filteredData)
+  res.json(filteredData);
+});
+app.listen(5200, () => console.log("the server is started on port 5200"));
