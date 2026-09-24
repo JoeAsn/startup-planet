bf4276219c3bbce657a9ce6b0821413e792a275f
Yohannes Asnake <asnakeyohannes095@gmail.com>
2026-09-24 10:47:28 +0300

    bugfix : make the filter flixible with the query params

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,42 @@
 import express from "express";
 import { startups } from "./Data/data.js";
+
 const app = express();
+
 app.get("/api", (req, res) => {
-  // const url = new URL(req.url, `http://${req.headers.host}`);
-  // let searchParams = Object.fromEntries(url.searchParams)
-  // console.log(searchParams) to extract the search params from the pure node
-  console.log(req.query); //searchParams express ;
+  console.log(req.query);
+
   const queryParams = req.query;
+
   let filteredData = startups.filter((data) => {
     for (let query in queryParams) {
       const key = query.toLowerCase();
 
       if (!(key in data)) return false;
-      if (data[key] !== queryParams[query]) return false;
+      if (typeof data[key] === "string") {
+        if (
+          data[key].toLowerCase() !==
+          queryParams[query].toLowerCase()
+        ) {
+          return false;
+        }
+      } else if (typeof data[key] === "boolean") {
+        if (data[key] !== (queryParams[query] === "true")) {
+          return false;
+        }
+      } else if (typeof data[key] === "number") {
+        if (data[key] !== Number(queryParams[query])) {
+          return false;
+        }
+      }
     }
 
     return true;
   });
-  console.log(filteredData)
+
   res.json(filteredData);
 });
-app.listen(5200, () => console.log("the server is started on port 5200"));
+
+app.listen(5200, () => {
+  console.log("The server is started on port 5200");
+});
